c879d108c1d3b6415af3549089ff2614ea345af0
jean-louis spatuzza <jlspatuzza@hotmail.fr>
2024-04-24 14:37:21 +0200

    this is the very first commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
   def home
+  end
+
+  def courses
+  end
+
+  def about
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: "pages#home"
+
+  get "courses", to: "pages#courses"
+
+  get "about", to: "pages#about"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
