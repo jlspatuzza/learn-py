e803e2523dd27ad2044a9f918b86c834d1d1a32d
jean-louis spatuzza <jlspatuzza@hotmail.fr>
2024-05-23 10:24:18 +0200

    last

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
+
+
   include Pundit
 
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
@@ -19,7 +21,8 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :status])
-
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:last_name])
 
   end
 
@@ -28,6 +31,8 @@ class ApplicationController < ActionController::Base
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
+
+  
 
 
 end
