79cbdd3026c3a3d38f269d360fdb2ed96d83ef2a
jean-louis spatuzza <jlspatuzza@hotmail.fr>
2024-05-28 13:45:39 +0200

    changing google console parameters

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -15,8 +15,7 @@ Devise.setup do |config|
   # ==> Omniauth
   config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
     scope: 'email,profile',
-    prompt: 'select_account',
-    redirect_uri: 'http://localhost:3000/users/auth/google_oauth2/callback'
+    prompt: 'select_account'
   }
 
 
